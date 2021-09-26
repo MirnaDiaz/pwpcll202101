4793a2ea9cd74c3a7aa70ba8f8c8fdddba8de523
Mirna Diaz <l171130309@gamadero.tecnm.mx>
2021-09-26 17:44:51 -0500

    Agregando nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Mirna Sanchez' });
+  res.render('index', { title: 'Express', author: 'Mirna Sanchez', appName: 'WebApp', company: 'Awsom Software' });
 });
 
 /* agregando nueva ruta */
