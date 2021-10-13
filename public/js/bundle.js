bc6aa15740a05b7735ca9dce82758e5ebb625427
Mirna Diaz <l171130309@gamadero.tecnm.mx>
2021-10-13 13:39:52 -0500

    Integrando Babel a webpack

---
+++ public/js/bundle.js
@@ -0,0 +1,1 @@
+console.log("Webpack Working!!!"),((o="hola")=>{alert(o)})(),async function(){console.log("Calling an async funtion");const o=await new Promise((o=>{setTimeout((()=>{o("resolved")}),2e3)}));console.log(o)}();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,53 @@
+const path = require('path');
 module.exports = {
+    //0. Establecer el modo del webpack
+    mode: 'development',
     //1. Especificando el archivo de entrada
     entry: './client/index.js',
-    //2. Especificar el archivo de salida
+    //2. Especificando la salida
     output: {
-        path: '/public',  //3. Ruta absoluta de la salida
-        filename: 'bundle.js' //4. Nombre del archivo de salida
-    },
-    devServer : {
-        static: './public'
+        //3. Ruta absoluta de salida
+        path: path.join(__dirname, 'public'),
+        //4. Nombre del archivo de salida
+        filename: 'js/bundle.js',
+        //5. Ruta del path publica para fines del servidor de desarrollo
+        publicPath: '/'
+    },    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        'modules': false,
+                                        'useBuiltIns': 'usage',
+                                        'targets': {"chrome": "80"},
+                                        'corejs': 3
+                                    }
+                                ]                               
+                            ],
+                            "plugins": [
+                                [
+                                    "module-resolver",
+                                    {
+                                        "root": ["./"],
+                                        "alias": {
+                                            "@client" : "./client",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
